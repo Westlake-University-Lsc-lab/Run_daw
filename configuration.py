563372694da4cbcab923b43d76f35f930463e0ca
yangjijun <yangjijun@westlake.edu.cn>
2025-09-17 15:07:31 +0800

    add parameter, and parameter number check function

--- configuration.py
+++ configuration.py
@@ -2,7 +2,13 @@ import ConfigPulseGen as config
 import argparse
 import sys
 
-# argvs = sys.argv
+
+def validate_args(args):
+    """验证所有必需参数都已提供"""
+    required_args = ['freq', 'amp', 'delay', 'sync', 'comb', 'C2_ON', 'LongS2']
+    for arg in required_args:
+        if getattr(args, arg) is None:
+            raise ValueError(f"Missing required argument: {arg}")
 
 def main():
     try:
@@ -11,19 +17,18 @@ def main():
         parser.add_argument('--amp', type=float, help='S1 pulse amplitude, 1.36V')
         parser.add_argument('--delay', type=int, help='delay time, 5us')
         parser.add_argument('--sync', type=str, help='Synchronize trigger Channel, CH1')
-        parser.add_argument('--Comb', type=str, help='CH1 Combine Mode option, True')
+        parser.add_argument('--comb', type=str, help='CH1 Combine Mode option, True')
         parser.add_argument('--C2_ON', type=str, help='CH2 Open on Option, False')
         parser.add_argument('--LongS2', type=str, help='Long2S2 200us Configure Option, False')
         args = parser.parse_args()  
 
-        if len(vars(args)) != 6:
-            raise Exception("Invalid number of arguments.")
-            return 0
+        validate_args(args)
+
         freq = args.freq
         amp = args.amp
         delay = args.delay
         sync = args.sync
-        comb = args.Comb
+        comb = args.comb
         C2_ON = args.C2_ON
         LongS2 = args.LongS2
         
@@ -36,8 +41,12 @@ def main():
         print("Long2S2 200us Configure:", LongS2)
         print("Arguments parsed successfully.")
         
-        ip = "10.11.50.78"
-        port = 5025
+        
+        ip = "192.168.4.9"
+        port = 5024
+
+        #ip = "10.11.50.78"
+        #port = 5025
         
         config.configure_waveform_generator(ip, port, freq, amp, delay, sync, comb=comb,C2_ON=C2_ON,LongS2=LongS2)
 
@@ -56,8 +65,13 @@ def main():
         sys.exit(0)
 
     except Exception as e:
-        print("An error occurred while parsing arguments:", str(e))
-        print("Usagee: python configuration.py --freq 50 --amp 1.4 --delay 5 --sync CH1 --comb True --C2_ON False --LongS2 False")        
+        print("Error:", str(e))
+        print("Usagee: python configuration.py --freq 50 --amp 1.4 --delay 5 --sync CH1 --Comb False --C2_ON False --LongS2 False")        
         sys.exit(0)
         
 
+
+if __name__ == "__main__":
+    main()
+
+    
